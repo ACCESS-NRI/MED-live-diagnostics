81ba2c2d78da3a05963e31b0c28ccab5e3347406
Mike Tetley <michaelgtetley@gmail.com>
2023-07-21 15:16:42 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../src/'))
 # sys.path.insert(0, '/Users/miket/Projects/005_MED-diagnostics/med-live_diagnostics/src')
 
 
