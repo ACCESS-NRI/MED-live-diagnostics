d5a4e8a8c83981608c7127883c4bca129731eed3
Mike Tetley <michaelgtetley@gmail.com>
2023-07-27 08:52:50 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
 
 html_theme_options = {
-    "use_edit_page_button": True,
+    "use_edit_page_button": False,
     "github_url": "https://github.com/ACCESS-NRI/MED-live-diagnostics",
     "logo": {
         "image_light": "_static/access_logo_rgb.svg",
