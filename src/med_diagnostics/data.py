c0b1fac06516f53ff2e0942cdb603ca97fa7cb98
Theo Ellingsen <83192769+theoellingsen@users.noreply.github.com>
2026-08-07 12:36:04 +1000

    test change for running through git usage (#36)
    * test change for running through git usage
    
    * Update src/med_diagnostics/data.py
    
    Co-authored-by: Charles Turner <52199577+charles-turner-1@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Charles Turner <52199577+charles-turner-1@users.noreply.github.com>

--- src/med_diagnostics/data.py
+++ src/med_diagnostics/data.py
@@ -10,7 +10,6 @@ import intake
 
 from access_nri_intake.source.builders import AccessCm2Builder # for use with access-med-env
 
-
         
 def _check_for_new_data(model_path, model_data, model_type):
     
